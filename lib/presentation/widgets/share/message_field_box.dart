f42ff44c4e8115afc45b11bec75a804ad05d87a7
Raul Ventura <u201714049@upc.edu.pe>
2023-10-15 23:50:29 -0500

    feat : new behavior added

--- lib/presentation/widgets/share/message_field_box.dart
+++ lib/presentation/widgets/share/message_field_box.dart
@@ -6,28 +6,42 @@ class MessageFieldBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final focusNode=FocusNode();
+    final textController=TextEditingController();
+
     final outlineInputBorder = UnderlineInputBorder(
       borderSide: const BorderSide(color: Colors.transparent),
       borderRadius: BorderRadius.circular(40)
     );
 
     final inputDecoration = InputDecoration(
+
         enabledBorder: outlineInputBorder,
+        hintText: 'End your message with a "?"',
         focusedBorder: outlineInputBorder,
         filled: true,
         suffixIcon: IconButton(
           icon: const Icon(Icons.send_outlined),
           onPressed: (){
-            print('Text box value?');
+            final textValue=textController.value.text;
+            print('Button: $textValue');
+            textController.clear();
           },
         )
     );
 
     return TextFormField(
-
+      onTapOutside: (event) {
+        focusNode.unfocus();//quita el teclado cuando doy click a otra parte de la pantalla
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('changed: $value');
+        textController.clear();
+        focusNode.requestFocus();//para qe continue apareciendo el teclado despues de
+                                  // darle done o chek
       },
     );
   }
